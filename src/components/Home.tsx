29bf1f25b84f65668bc10d51993a7dc73addf295
ajesh_151 <143197654+ajesh151@users.noreply.github.com>
2024-12-12 20:03:41 +0000

    last commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ const generateAnonymousId = (userId: string) => {
     for (let i = 0; i < str.length; i++) {
       const char = str.charCodeAt(i);
       hash = (hash << 5) - hash + char;
-      hash = hash & hash; // Convert to 32-bit integer
+      hash = hash & hash;
     }
     return Math.abs(hash);
   };
